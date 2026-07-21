48f74ed664d5a0b38e188e3e4e5ec9db9141f915
wzbis666 <3378621722@qq.com>
2026-07-21 23:24:41 +0800

    fix: remove service worker causing white screen on revisit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 
-// Register service worker for PWA
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').catch(() => {});
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
